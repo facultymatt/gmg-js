5595154f19312dd3a9f1819cc19c4dc8f5037419
Matt Miller <mattmillerart+github@gmail.com>
2020-04-05 22:27:46 -0700

    Initial commit

---
+++ GrillStatus.js
@@ -0,0 +1,84 @@
+const getRawValue = (hex, position) => {
+  const value = hex.substr(position, 2)
+  const parsed = parseInt(value, 16)
+  return parsed
+}
+
+const getGrillState = (hex) => {
+  const statusCharacter = hex.charAt(61)
+  let status = parseInt(statusCharacter, 10)
+  if (status === 0) status = 'off'
+  else if (status === 1) status = 'on'
+  else if (status === 2) status = 'fan mode'
+  else status = 'unknown'
+  return status
+}
+
+const getCurrentGrillTemp = (hex) => {
+  const first = getRawValue(hex, 4)
+  const second = getRawValue(hex, 6)
+  return first + (second * 256)
+}
+
+const getLowPelletAlarmActive = (hex) => {
+  const first = getRawValue(hex, 48)
+  const second = getRawValue(hex, 50)
+  const value = first + (second * 256)
+  return value === 128
+}
+
+// 12 on chart, next is 16
+const getDesiredGrillTemp = (hex) => {
+  const first = getRawValue(hex, 12)
+  const second = getRawValue(hex, 14)
+  return first + (second * 256)
+}
+
+const getCurrentFoodTemp = (hex) => {
+  const first = getRawValue(hex, 8)
+  const second = getRawValue(hex, 10)
+  const currentFoodTemp = first + (second * 256)
+  return currentFoodTemp >= 557 ? 0 : currentFoodTemp
+}
+
+const test = (hex, start) => {
+  const first = getRawValue(hex, start)
+  const second = getRawValue(hex, start + 2)
+  return first + (second * 256)
+}
+
+
+const getDesiredFoodTemp = (hex) => {
+  const first = getRawValue(hex, 56)
+  const second = getRawValue(hex, 58)
+  return first + (second * 256)
+}
+
+class GrillStatus {
+  constructor(bytes) {
+    const hex = Buffer.from(bytes).toString('hex')
+    this.state = getGrillState(hex)
+    this._hex = hex
+    this.isOn = this.state === 'on'
+    this.currentGrillTemp = getCurrentGrillTemp(hex)
+    this.desiredGrillTemp = this.isOn ? getDesiredGrillTemp(hex) : 0
+    this.currentFoodTemp = getCurrentFoodTemp(hex)
+    this.desiredFoodTemp = this.isOn ? getDesiredFoodTemp(hex) : 0
+    this.fanModeActive = this.state === 'fan mode'
+    this.lowPelletAlarmActive = getLowPelletAlarmActive(hex)
+
+    this.test_16 = test(hex, 16);
+    this.test_20 = test(hex, 20);
+    this.test_24 = test(hex, 24);
+    this.test_28 = test(hex, 28);
+    this.test_32 = test(hex, 32);
+    this.test_36 = test(hex, 36);
+    this.test_40 = test(hex, 40);
+    this.test_44 = test(hex, 44);
+    this.test_52 = test(hex, 52);
+    this.test_64 = test(hex, 64);
+    this.test_68 = test(hex, 68);
+  }
+}
+
+module.exports = GrillStatus
---
+++ index.js
@@ -0,0 +1,45 @@
+const dgram = require("dgram");
+const express = require("express");
+const GrillStatus = require('./GrillStatus')
+
+const app = express();
+const socket = dgram.createSocket("udp4");
+
+const IP = "192.168.86.134";
+const PORT = 8080;
+
+const commands = Object.freeze({
+  powerOn: 'UK001!',
+  // powerOff: 'UK004!',
+  getGrillStatus: 'UR001!',
+  getGrillId: 'UL!',
+  // setGrillTempF: (temp) => `UT${temp}!`,
+  setFoodTempF: (temp) => `UF${temp}!`
+});
+
+// socket.setBroadcast(true);
+
+socket.on("message", (msg, info) => {
+  console.log('message', msg, info);
+  const buff = Buffer.from(msg);
+  // console.log('message', buff.toString());
+  const status = new GrillStatus(buff);
+  console.log(status);
+});
+
+const doSend = (message) => {
+  const data = Buffer.from(message, 'ascii')
+  socket.send(data, 0, data.byteLength, PORT, IP, (error) => {
+    console.error(error);
+  });
+}
+
+// doSend(commands.getGrillId);
+doSend(commands.getGrillStatus);
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
